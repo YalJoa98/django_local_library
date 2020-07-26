dbb15ef79b55d97256292354357a7c2f1b4f26a1
Yalser Isaac Ramirez Guzman <isaacdragon2013@gmail.com>
2020-07-25 23:23:00 -0500

    Modificacion settings.py

--- MariaBonita/urls.py
+++ MariaBonita/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'mariabonita'
 
 urlpatterns = [
-    path('index/', views.index, name="index"),
+    path('', views.index, name="index"),
     path('productos/', views.productos, name="productos"),
     path('categorias/', views.categorias, name="categorias"),
 ]
