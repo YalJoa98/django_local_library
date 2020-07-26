a38592d479d98674192aa85111b641b73227b9d5
Yalser Isaac Ramirez Guzman <isaacdragon2013@gmail.com>
2020-07-26 10:09:28 -0500

    Se modifico models.py

--- MariaBonita/urls.py
+++ MariaBonita/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('productos/', views.productos, name="productos"),
     path('categorias/', views.categorias, name="categorias"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
