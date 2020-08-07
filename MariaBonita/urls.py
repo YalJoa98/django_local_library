8b640c970a5f6dc4dd352ae9b9de5f090f95ec3d
Yalser Isaac Ramirez Guzman <isaacdragon2013@gmail.com>
2020-08-06 22:36:14 -0500

    Template Productos

--- MariaBonita/urls.py
+++ MariaBonita/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 app_name = 'mariabonita'
 
 urlpatterns = [
-    path('index/', views.Index.as_view(), name="index"),
+    path('', views.Index.as_view(), name="index"),
     path('productos/', views.Producto.as_view(), name="productos"),
     path('categorias/', views.Categorias.as_view(), name="categorias"),
     path('resultados/',views.Resultados.as_view(), name="buscar"),
